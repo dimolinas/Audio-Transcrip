1c1251a4794f360bd01ee23d9a8ed07d9b837fcd
diegoe012 <55567041+dimolinas@users.noreply.github.com>
2022-06-11 22:43:58 -0500

    Update request.py

--- request.py
+++ request.py
@@ -1,4 +1,6 @@
 import requests
+#state of request
+#https://app.assemblyai.com/processing-queue
 endpoint = "https://api.assemblyai.com/v2/transcript"
 json = {
     "audio_url": "https://cdn.assemblyai.com/upload/ad55b3a4-e896-4a3f-8cae-88fedbf770f3",
